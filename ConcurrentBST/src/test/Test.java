4b0e76cc9dbd6353dd6b09197380c5421d2f8c70
LoreBz <lolloghironf@hotmail.it>
2015-11-01 22:36:53 +0100

    Print dell'albero in consolle

--- ConcurrentBST/src/test/Test.java
+++ ConcurrentBST/src/test/Test.java
@@ -27,10 +27,16 @@ public class Test {
 
 //        tree.insert(2, "A");
 //        
-        tree.insert(1, "B");
-        tree.insert(3, "C");
-        tree.insert(5, "F");
-        tree.insert(3,"D");
+        tree.insert(1, 1+"");
+        tree.insert(3, 3+"");
+        tree.insert(5, 5+"");
+        tree.insert(3,3+"");
+        tree.insert(4, 4+"");
+        tree.insert(7, 7+"");
+        tree.insert(12, 12+"");
+//        tree.insert(10, 10+"");
+//        tree.insert(14, 14+"");
+//        BTreePrinter.printNode(tree.getRoot());
 //        
 //        tree.delete(5);
 //        tree.delete(1);
